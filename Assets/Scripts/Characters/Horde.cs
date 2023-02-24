3896c732738c7ca6fcad3d915688c9e34f68a08f
Vladislav <vlad.vavilin73@gmail.com>
2023-02-24 17:37:14 +0300

    Added Horde class

---
+++ Assets/Scripts/Characters/Horde.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters
+{
+    public class Horde : MonoBehaviour
+    {
+        
+        // private                  Transform _transform = default;
+        [SerializeField] private Vector2   _direction = Vector2.right;
+        
+        public Transform Transform { get; private set; }
+        
+        private void Awake()
+        {
+            Transform = GetComponent<Transform>();
+        }
+
+        private void Update()
+        {
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Events/IntEventChannelSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Events
+{
+    [CreateAssetMenu(fileName = "Int Event Channel",
+                     menuName = "Events/Int Event Channel",
+                     order = 0)]
+    public class IntEventChannelSO : ScriptableObject
+    {
+        
+    }
+}
