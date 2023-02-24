8f3594a9eb0d1933cb8ca21632cee69dcd720553
Vladislav <vlad.vavilin73@gmail.com>
2023-02-24 22:38:41 +0300

    Some managers
    * Room Camera
    * Room Pool Manager - ?

---
+++ Assets/Scripts/Camera/RoomCamera.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+namespace Camera
+{
+    [RequireComponent(typeof(Collider2D),
+                      typeof(Rigidbody2D))]
+    public class RoomCamera : MonoBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera _roomCamera;
+
+        private void Awake()
+        {
+            _roomCamera = GetComponent<CinemachineVirtualCamera>();
+
+            if (_roomCamera == null)
+            {
+                throw new InvalidProgramException();
+            }
+        }
+
+        private void Start()
+        {
+            _roomCamera.enabled = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            _roomCamera.enabled = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            _roomCamera.enabled = false;
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (_roomCamera.enabled == false) _roomCamera.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Events/GameObjectEventChannelSO.cs
@@ -0,0 +1,13 @@
+﻿using BaseClasses.ScriptableObjects.Events;
+using UnityEngine;
+
+namespace Events
+{
+    [CreateAssetMenu(fileName = "Game Object Event Channel",
+                     menuName = "Events/Game Object Event Channel",
+                     order = 0)]
+    public class GameObjectEventChannelSO : TypedEventChannelSO<GameObject>
+    {
+        
+    }
+}
