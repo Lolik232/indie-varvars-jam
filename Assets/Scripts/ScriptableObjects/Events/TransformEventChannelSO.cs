d24c99194ded581d36a2712956db1d6d188bcfd4
Vladislav <vlad.vavilin73@gmail.com>
2023-02-24 15:01:29 +0300

    Events added

---
+++ Assets/Scripts/ScriptableObjects/Events/TransformEventChannelSO.cs
@@ -0,0 +1,12 @@
+﻿using BaseClasses.ScriptableObjects.Events;
+using UnityEngine;
+
+namespace Events
+{
+    [CreateAssetMenu(fileName = "Transform Event Channel", 
+                     menuName = "Events/Transform Event Channel",
+                     order = 0)]
+    public class TransformEventChannelSO : TypedEventChannelSO<Transform>
+    {
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Events/VoidEventChannelSO.cs
@@ -0,0 +1,12 @@
+﻿using BaseClasses.ScriptableObjects.Events;
+using UnityEngine;
+
+namespace Events
+{
+    [CreateAssetMenu(fileName = "VoidEventChannel", 
+                     menuName = "Events/Void Event Channel", 
+                     order = 0)]
+    public class VoidEventChannelSO : EventChannelSO
+    {
+    }
+}
