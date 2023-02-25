f19623bbc4514e6168789e70dfa93284f0f2ab90
Vladislav <vlad.vavilin73@gmail.com>
2023-02-25 13:10:38 +0300

    Maybe parallax

---
+++ Assets/Scripts/Game/RoomSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class RoomSpawner : MonoBehaviour
+    {
+        
+        
+        
+    }
+}
---
+++ Assets/Scripts/Room/Room.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class Room : MonoBehaviour
+{
+    [Header("SETTINGS")]
+    [SerializeField] private Transform _startRoomPoint;
+    [SerializeField] private Transform _endRoomPoint;
+    
+    
+    
+}
