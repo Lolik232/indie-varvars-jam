7d71019558992e766a7bb99a6ad61282702706dd
Fadet <tntboxmc@gmail.com>
2023-02-26 12:28:49 +0300

    Start menu final fix

---
+++ Assets/Scripts/Audio/VolumeManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] private Slider gameSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider fxSlider;
+    [SerializeField] private AudioMixerGroup mixer;
+
+    private void Start()
+    {
+        gameSlider.value = PlayerPrefs.GetFloat("GameVolume", 1f);
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        fxSlider.value = PlayerPrefs.GetFloat("FXVolume", 1f);
+    }
+
+    private void ChangeVolume(string name, float volume)
+    {
+        mixer.audioMixer.SetFloat(name, SqrtInterpolation(-80, 0, volume));
+        PlayerPrefs.SetFloat(name, volume);
+    }
+    
+    private static float SqrtInterpolation(float a, float b, float t)
+    {
+        return a + (b - a) * (float) Math.Sqrt(t);
+    }
+    
+    public void OnGameSlider(float volume)
+    {
+        ChangeVolume("GameVolume", volume);
+    }
+
+    public void OnMusicSlider(float volume)
+    {
+        ChangeVolume("MusicVolume", volume);
+    }
+
+    public void OnFXSlider(float volume)
+    {
+        ChangeVolume("FXVolume", volume);
+    }
+}
--- Assets/Scripts/UI/UIInput.cs
+++ Assets/Scripts/UI/UIInput.cs
@@ -3,9 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIInput : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+
+    private void Start()
+    {
+        startButton.Select();
+    }
+
     public void OnPlayButton()
     {
         throw new NotImplementedException();
