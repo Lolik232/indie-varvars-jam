03c9b5ee0331bfe89942ebfa2e58ea4115747208
Fadet <tntboxmc@gmail.com>
2023-02-26 03:10:01 +0300

    Start menu implementation

---
+++ Assets/Scripts/UI/UIInput.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIInput : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
